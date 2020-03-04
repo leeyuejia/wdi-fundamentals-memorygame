dccf5bd9bbad6691bc78a3ef8f725d2600630408
YUE JIA <leeyuejia@gmail.com>
2020-03-04 21:38:49 +0800

    created game logic

--- memory_game/all memory_game files/js/main.js
+++ memory_game/all memory_game files/js/main.js
@@ -1,9 +1,21 @@
 
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+cardsInPlay;
+const cardThree = cards[0];
+cardsInPlay.push(cardThree);
+cardsInPlay;
+
+console.log();
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+
+if (cardsInPlay.length === 2);
+if (cardsInPlay[0] === cardsInPlay [2]) {alert("You Found a match!")} else {alert ("Sorry,try again")};
+
+
