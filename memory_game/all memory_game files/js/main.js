2236d1f58443d0d0aec803c68efcd78bb3db5a78
YUE JIA <leeyuejia@gmail.com>
2020-03-06 12:31:13 +0800

    final game draft (not working)

--- memory_game/all memory_game files/js/main.js
+++ memory_game/all memory_game files/js/main.js
@@ -1,27 +1,66 @@
 
 
 
-const cards = ["queen", "queen", "king", "king"];
+/*const cards = ["queen", "queen", "king", "king"];*/
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	},
+];
+
+
 const cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+	//if (cardsInPlay.length >= 2) {//
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  		alert("You found a match!");
 	} else {
-  console.log("Sorry, try again.");
-	}
-}
-
-function flipCard(cardId) {
-	console.log( "User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	
+  		alert("Sorry, try again.");
+	};
 };
-flipCard(0);
-flipCard(2);
-checkForMatch();
 
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	//console.log( "User flipped " + cards[cardId].rank);//
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	//console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	if (cardInPlay.length === 2) {
+		checkForMatch();
+	//	cardsInPlay = [];
+	};
+};
 
+function createBoard() {
+	const cardTable = document.getElementById('game-board');
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		cardTable.appendchild(cardElement);
+	}
+};
+createBoard();
 
 	/*const cardOne = cards[0];
 	cardsInPlay.push(cardOne);
